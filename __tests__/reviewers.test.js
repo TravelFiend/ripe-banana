d703c5c584c6401e32052bbb26009db15c66aa2a
TravelFiend <mikegrace274@gmail.com>
2020-01-09 00:11:53 -0800

    reviewers test refactoring complete

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -1,4 +1,5 @@
-const { getReviewer, getReviewers, getReview } = require('../lib/helpers/data-helpers');
+const { getReviewer, getReviewers, getReview, getReviews } = require('../lib/helpers/data-helpers');
+const Review = require('../lib/models/Review');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -38,54 +39,48 @@ describe('reviewer routes', () => {
     });
 
     it('should get a reviewer by id', async() => {
-        await getReview();
         const reviewer = await getReviewer();
+        const reviews = await getReviews({ reviewer: reviewer._id });
 
         return request(app)
             .get(`/api/v1/reviewers/${reviewer._id}`)
             .then(res => {
+                expect(res.body.reviews).toHaveLength(reviews.length);
                 expect(res.body).toEqual({
                     _id: expect.any(String),
-                    name: 'MikeEG',
-                    company: 'Chinchiller',
-                    reviews: [{
-                        _id: expect.any(String),
-                        rating: 4,
-                        review: 'A movie about absolutely nothing',
-                        film: {
-                            _id: expect.any(String),
-                            title: 'A movie'
-                        }
-                    }]
-                });
-            });
-    });
-
-    it('should delete a reviewer if they have no reviews', async() => {
-        const reviewer = await getReviewer();
-
-        return request(app)
-            .delete(`/api/v1/reviewers/${reviewer._id}`)
-            .then(res => {
-                expect(res.body).toEqual({
-                    _id: expect.any(String),
-                    name: 'MikeEG',
-                    company: 'Chinchiller',
-                    __v: 0
+                    name: reviewer.name,
+                    company: reviewer.company,
+                    reviews: expect.any(Array)
                 });
             });
     });
 
     it('should not delete a reviewer if they have reviews', async() => {
-        const reviewer = await getReviewer();
-        await getReview();
-
+        const review = await getReview();
+        const reviewer = await getReviewer({ _id: review.reviewer });
+        
         return request(app)
             .delete(`/api/v1/reviewers/${reviewer._id}`)
             .then(res => {
                 expect(res.body).toEqual({
-                    message: 'Didn\'t work',
+                    message: 'Unable to delete, reviewer has active reviews',
                     status: 500
+                });
+            });
+    });
+
+    it('should delete a reviewer if they have no reviews', async() => {
+        const reviewer1 = await getReviewer();
+        await Review.deleteMany({ reviewer: reviewer1._id });
+
+        return request(app)
+            .delete(`/api/v1/reviewers/${reviewer1._id}`)
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    name: reviewer1.name,
+                    company: reviewer1.company,
+                    __v: 0
                 });
             });
     });
